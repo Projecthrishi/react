40a4da4e5c6797996f9123d212e3285372cadcf8
Projecthrishi <114040477+Projecthrishi@users.noreply.github.com>
2024-05-04 19:42:48 +0530

    Add files via upload

---
+++ conf/conf.js
@@ -0,0 +1,15 @@
+const conf={
+    appwriteurl:String(import.meta.env.VITE_APPWRITE_URL),
+    appWriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwritecollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwritedatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwritebucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    
+
+
+
+
+}
+
+
+export default conf
